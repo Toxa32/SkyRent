b02f89c792f9c548acddf8d3aeba4d0ccf3de621
Aleksey Mavrin <98215057+plamer777@users.noreply.github.com>
2023-01-08 23:44:18 +0300

    Update run.py
    change config settings

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 """This is a main file to start the application"""
 import dotenv
 from flask_cors import CORS
-from configs import ConfigManager
+from configs import DevelopConfig
 from views.real_estate import real_estate_ns
 from views.locations import location_ns
 from utils import create_app, add_namespaces, api
 # -------------------------------------------------------------------------
 dotenv.load_dotenv()
 # ------------------------------------------------------------------------
-config = ConfigManager().get_config()
+config = DevelopConfig()
 app = create_app(config)
 add_namespaces(api, [real_estate_ns, location_ns])
 CORS(app)
